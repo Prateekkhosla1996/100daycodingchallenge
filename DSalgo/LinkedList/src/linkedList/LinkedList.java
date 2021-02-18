b53842cd91816c8eec8c6f32cbfd2968807860fc
prateek khosla <prateekk96.com@gmail.com>
2021-02-19 01:31:53 +0530

    fifth Day

--- DSalgo/LinkedList/src/linkedList/LinkedList.java
+++ DSalgo/LinkedList/src/linkedList/LinkedList.java
@@ -217,6 +217,22 @@ public class LinkedList {
 	          ri--;
 	      }
 	    }
-	
+	public void reversePI(){
+	      // write your code here
+	      
+	      Node prev = null;
+	      Node curr=head;
+	      while(curr!=null){
+	      Node ocn=curr.next;
+	          curr.next=prev;
+	          
+	          prev=curr;
+	          curr=ocn;
+	      }
+	      Node temp = head;
+	      head=tail;
+	      tail=temp;
+	    }
+	  
 	
 }
