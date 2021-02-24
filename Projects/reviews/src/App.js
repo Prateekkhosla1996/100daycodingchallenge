6cf9a6ade4307da9eb14ad66bc85db6ad6be1e6e
prateek khosla <prateekk96.com@gmail.com>
2021-02-25 02:34:01 +0530

    eleventh day reviews project

--- Projects/reviews/src/App.js
+++ Projects/reviews/src/App.js
@@ -1,10 +1,17 @@
 import React from 'react'
 import Review from './Review';
+import {FaGithubSquare} from 'react-icons/fa'
 function App() {
   return (
-    <div>
-      
-    </div>
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our reviews</h2>
+          <div className="underline"></div>
+        </div>
+        <Review/>
+      </section>
+      </main>
   )
 }
 
