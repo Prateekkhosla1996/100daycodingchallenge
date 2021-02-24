045411a6242c0753df4ee76dc9b769a8eeb6b779
prateek khosla <prateekk96.com@gmail.com>
2021-02-25 04:18:57 +0530

    eleventh day accordion complete

--- Projects/accordion/src/App.js
+++ Projects/accordion/src/App.js
@@ -1,10 +1,19 @@
-import React from 'react'
-
+import React ,{ useState } from 'react'
+import questions from './data';
+import data from './data'
+import Question from './Question';
+import SingleQuestion from './Question';
 function App() {
+  const [questions,setQuestion]=useState(data);
   return (
-    <div>
-      
-    </div>
+    <main>
+      <div className="container">
+        <h3>Question and answer about login</h3>
+        <section className="info">{questions.map((question)=>{
+          return <SingleQuestion key={question.id} {...question}/>
+        })}</section>
+      </div>
+    </main>
   )
 }
 
