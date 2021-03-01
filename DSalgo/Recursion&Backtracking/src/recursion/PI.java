eb5e6ee53ddca60110d89c287dcb3a7746a7eea5
prateek khosla <prateekk96.com@gmail.com>
2021-03-01 13:36:12 +0530

    Fifteenth day recursion basics

--- DSalgo/Recursion&Backtracking/src/recursion/PI.java
+++ DSalgo/Recursion&Backtracking/src/recursion/PI.java
@@ -1,5 +1,22 @@
 package recursion;
+import java.io.*;
+import java.util.*;
 
 public class PI {
+	
+	    public static void main(String[] args) throws Exception {
+	        // write your code here
+	         Scanner sc = new Scanner(System.in);
+		        int n = sc.nextInt();
+		        printIncreasing(n);
+	    }
 
-}
+	    public static void printIncreasing(int n){
+	        if(n==0){
+	            return;
+	        }
+	        printIncreasing(n-1);
+	        System.out.println(n);
+	    }
+
+	}
