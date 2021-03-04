0472c42ccb6864b92b4ada985af17a240de7b018
prateek khosla <prateekk96.com@gmail.com>
2021-03-05 04:33:34 +0530

    nineteenth day recursion print recursion questions

---
+++ DSalgo/Recursion&Backtracking/src/recursion/PrintPermutation.java
@@ -0,0 +1,26 @@
+package recursion;
+import java.io.*;
+import java.util.*;
+
+public class PrintPermutation {
+
+	    public static void main(String[] args) throws Exception {
+	        Scanner sc = new Scanner(System.in);
+	        String str = sc.nextLine();
+	            printPermutations(str,"");
+	        
+	    }
+
+	    public static void printPermutations(String str,String ans) {
+	        if(str.length()==0){
+	            System.out.println(ans);
+	            return;
+	        }
+	        for(int i = 0; i < str.length() ; i++){
+	            char ch = str.charAt(i);
+	            String ros = str.substring(0,i)+str.substring(i+1);
+	            printPermutations(ros,ans+ch);
+	        }
+	    }
+
+	}
