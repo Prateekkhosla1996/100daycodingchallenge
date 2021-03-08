213f2ae62d096edcef4882716e85a26fb571787e
prateek khosla <prateekk96.com@gmail.com>
2021-03-08 20:54:00 +0530

    day 22 Lecture 7

---
+++ NagarroWebDEVBOOTCAMP/Lecture 7/JSInro/index.js
@@ -0,0 +1,18 @@
+// variables 
+var a = 10;
+let b = 20;
+const c = 30;
+
+// array
+let arr=new Array(50,60,70);
+
+arr.push(10);
+arr.push(20);
+arr.push(30);
+
+console.log(arr);
+console.log(arr.length);
+
+for(let i=0;i<arr.length;i++){
+    console.log(arr[i]);
+}
