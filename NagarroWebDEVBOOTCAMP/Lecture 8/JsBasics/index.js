dc12b13bb4fffe1c48bc59e3c37830a648d70bb7
prateek khosla <prateekk96.com@gmail.com>
2021-03-10 20:04:58 +0530

    day 24 nagarro bootcamp lecture 8

--- NagarroWebDEVBOOTCAMP/Lecture 8/JsBasics/index.js
+++ NagarroWebDEVBOOTCAMP/Lecture 8/JsBasics/index.js
@@ -7,9 +7,36 @@
 // null
 
 var count = 0;
-
+// const pi = 3.14;
 while(count<=100){
     console.log(count);
     count++;
 }
 const pi=3.14;
+
+var isTrue = true;
+var bool = false;
+console.log(typeof(bool));
+console.log(typeof(pi));
+console.log(typeof(count));
+let str = "prateek";
+console.log(typeof(str));
+let name="";
+console.log(typeof(name));
+console.log(typeof(null));
+console.log(typeof(undefined));
+console.log(`this is prateek's book`)
+console.log('this is prateek\'s book')
+console.log("this is \"prateek\'s\" book")
+
+console.log(str.length)
+console.log(str[0]);
+console.log(str.charAt(3));
+console.log(str.substr(1))
+console.log(str.concat(1,4,66))
+console.log(str.length)
+console.log(['a',...str])
+console.log([1,...str])
+console.log(str.toUpperCase())
+console.log(str.toLocaleLowerCase())
+console.log(str.toLocaleUpperCase())
