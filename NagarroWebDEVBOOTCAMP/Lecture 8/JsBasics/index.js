5cb459067dc2da729a786640bd18ffeae9f580f7
prateek khosla <prateekk96.com@gmail.com>
2021-03-10 19:27:31 +0530

    day 24 nagarro bootcamp lecture 8

---
+++ NagarroWebDEVBOOTCAMP/Lecture 8/JsBasics/index.js
@@ -0,0 +1,15 @@
+// alert("welcome");
+// premitive
+// numbers
+// boolean
+//string
+//undefined
+// null
+
+var count = 0;
+
+while(count<=100){
+    console.log(count);
+    count++;
+}
+const pi=3.14;
