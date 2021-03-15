a97903d47337fee6c5e7a0a3c39a2eccc963c0b0
prateek khosla <prateekk96.com@gmail.com>
2021-03-16 04:05:27 +0530

    day 30

--- NagarroWebDEVBOOTCAMP/Lecture 9/Functions/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 9/Functions/app.js
@@ -37,3 +37,23 @@ function countWords(str){
     return count+1;
 }
 console.log(countWords(str))
+
+function isPrime(n){
+    if(n<=1){
+        return false;
+    }
+    for(let i=2;i<=Math.sqrt(n);i++){
+        if(n%i==0) return false;
+    }
+    return true;
+}
+console.log(isPrime(5))
+
+function printPrimes(n){
+    for(let i=2;i<=n;i++){
+        if(isPrime(i)){
+            console.log(i);
+        }
+    }
+}
+printPrimes(10)
