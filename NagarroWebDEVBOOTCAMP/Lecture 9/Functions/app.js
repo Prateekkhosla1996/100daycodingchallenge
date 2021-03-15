ee943664354fc26de3ba42074b3d40cf4f5db67e
prateek khosla <prateekk96.com@gmail.com>
2021-03-16 03:02:47 +0530

    day 30

--- NagarroWebDEVBOOTCAMP/Lecture 9/Functions/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 9/Functions/app.js
@@ -24,3 +24,16 @@ function a(){
     b()
 }
 a();
+
+let str= "this is a random string wow good "
+
+function countWords(str){
+    let count = 0;
+    for(let i=0;i<str.length;i++){
+        if(str.charAt(i)==' '){
+            count++;
+        }
+    }
+    return count+1;
+}
+console.log(countWords(str))
