906acc70d918b7333ac82ab7aa1a1cbb93abcd92
prateek khosla <prateekk96.com@gmail.com>
2021-03-16 02:46:49 +0530

    day 30

--- NagarroWebDEVBOOTCAMP/Lecture 9/Functions/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 9/Functions/app.js
@@ -1,11 +1,26 @@
 
-function sum(){
-    console.log(10+20);
-}
-function table(n){
-    for(let i=1;i<=10;i++){
-        console.log(`${n} x ${i}= ${n*i}`)
+// function sum(){
+//     console.log(10+20);
+// }
+// function table(n){
+//     for(let i=1;i<=10;i++){
+//         console.log(`${n} x ${i}= ${n*i}`)
+//     }
+// }
+// table(17)
+// sum();
+
+function a(){
+    
+    function b(){
+        
+        function c(){
+            console.log("inside c");
+        }
+        console.log('inside b')
+        c()
     }
+    console.log('inside a')
+    b()
 }
-table(17)
-sum();
+a();
