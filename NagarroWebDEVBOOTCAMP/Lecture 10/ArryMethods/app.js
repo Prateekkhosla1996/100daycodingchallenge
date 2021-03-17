e91b5ff77a044e7ac8a6b42707681b24681a5824
prateek khosla <prateekk96.com@gmail.com>
2021-03-18 02:14:11 +0530

    day 32 array Methods

--- NagarroWebDEVBOOTCAMP/Lecture 10/ArryMethods/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 10/ArryMethods/app.js
@@ -16,3 +16,25 @@ function isOdd(num){
 }
 let res = arr3.filter(isOdd)
 console.log(res)
+
+let menu=["kadhai paneer","Dal makhni","Chicken changezi","Dum aloo","Veg biryani",
+"butter chicken","onion rings", "garlic noodles","garlic nan","egg roll","rajma chawal"
+,"chicken roll"
+];
+function isVeg(food){
+    if(food.toLowerCase().indexOf('chicken')!==-1||food.toLowerCase().indexOf('egg')!==-1){
+        return false;
+    }
+    return true;
+}
+let veg = menu.filter(isVeg)
+console.log(veg)
+
+function isJain(food){
+    if(food.toLowerCase().indexOf('chicken')!==-1||food.toLowerCase().indexOf('egg')!==-1||food.toLowerCase().indexOf('onion')!==-1||food.toLowerCase().indexOf('garlic')!==-1){
+        return false;
+    }
+    return true;
+}
+let jain = menu.filter(isJain)
+console.log(jain)
