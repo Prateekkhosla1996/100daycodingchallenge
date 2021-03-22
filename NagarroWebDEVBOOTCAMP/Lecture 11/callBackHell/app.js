a10b5b477eb980b2db4a513c46cdb972aa19b262
prateek khosla <prateekk96.com@gmail.com>
2021-03-22 05:53:17 +0530

    day 36 resolving call back hell using promise

--- NagarroWebDEVBOOTCAMP/Lecture 11/callBackHell/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 11/callBackHell/app.js
@@ -3,47 +3,47 @@
 
 
 
-function downloadFile(url,downloaded){
-    console.log(`starting the download from ${url}`)
-    setTimeout(()=>{
-        const path = url.split('/').pop();
+// function downloadFile(url,downloaded){
+//     console.log(`starting the download from ${url}`)
+//     setTimeout(()=>{
+//         const path = url.split('/').pop();
         
-        downloaded(path)
-    },3000)
-}
-function compressFile(path,compressed){
-    console.log(`starting the file compression for ${path}`)
-    setTimeout(()=>{
-        const comp = path.split('.')[0]+'.zip';
-        compressed(comp);
-    },3000)
-}
-function uploadFile(compressed,uploaded){
-    console.log(`starting to upload the file from ${compressed}`)
-    setTimeout(()=>{
-        const uploadPath = `http://localhost/${compressed}`
-        uploaded(uploadPath)
-    },2000)
-}
-downloadFile('http://facebook.com/profile.jpg',function(path){ 
-    console.log('download complete as ' +path);
-    compressFile(path,function(compressed){
-        console.log(`file compressed as ${compressed}`)
-        uploadFile(compressed,function(uploaded){
-            console.log(`file uploaded sucessfully at ${uploaded}`)
-        })
-    })
+//         downloaded(path)
+//     },3000)
+// }
+// function compressFile(path,compressed){
+//     console.log(`starting the file compression for ${path}`)
+//     setTimeout(()=>{
+//         const comp = path.split('.')[0]+'.zip';
+//         compressed(comp);
+//     },3000)
+// }
+// function uploadFile(compressed,uploaded){
+//     console.log(`starting to upload the file from ${compressed}`)
+//     setTimeout(()=>{
+//         const uploadPath = `http://localhost/${compressed}`
+//         uploaded(uploadPath)
+//     },2000)
+// }
+// downloadFile('http://facebook.com/profile.jpg',function(path){ 
+//     console.log('download complete as ' +path);
+//     compressFile(path,function(compressed){
+//         console.log(`file compressed as ${compressed}`)
+//         uploadFile(compressed,function(uploaded){
+//             console.log(`file uploaded sucessfully at ${uploaded}`)
+//         })
+//     })
    
-})
+// })
 // .......................................good way using promise.....................................
 
 
 
-function downloadFile(url){
-    console.log(`starting the download from ${url}`)
+function downloadFile(url1){
+    console.log(`starting the download from ${url1}`)
     return new Promise((resolve,reject)=>{
         setTimeout(()=>{
-            const path = url.split('/').pop();
+            const path = url1.split('/').pop();
 
             resolve(path);
         },3000)
@@ -68,15 +68,21 @@ function uploadFile(compressed){
     })
 }
 
+// downloadFile('http://facebook.com/prateek.jpg')
+// .then((path)=>{
+//     console.log(`file downloaded sucessfully as ${path}`)
+//     compressFile(path).then((comp)=>{
+//         console.log(`file compressed successfully as ${comp}`)
+//         uploadFile(comp).then((uploadPath)=>{
+//             console.log(`file is uploaded at ${uploadPath}`)
+//         })
+//     })  
+// })
+
+
 downloadFile('http://facebook.com/prateek.jpg')
-.then((path)=>{
-    console.log(`file downloaded sucessfully as ${path}`)
-    compressFile(path).then((comp)=>{
-        console.log(`file compressed successfully as ${comp}`)
-        uploadFile(comp).then((uploadPath)=>{
-            console.log(`file is uploaded at ${uploadPath}`)
-        })
-    })  
+.then(compressFile)
+.then(uploadFile)
+.then((uploadPath)=>{
+    console.log(`file is uploaded at ${uploadPath}`)
 })
-
-
