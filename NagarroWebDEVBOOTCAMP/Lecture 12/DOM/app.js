51099c61f50981af1456fd582a41e71fc3eb073c
prateek khosla <prateekk96.com@gmail.com>
2021-03-23 05:51:27 +0530

    day 37 nagarro Lecture 12

--- NagarroWebDEVBOOTCAMP/Lecture 12/DOM/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 12/DOM/app.js
@@ -2,7 +2,7 @@ const heading = document.getElementsByTagName('h1')[0];
 const para = document.getElementById('para');
 const inner = document.getElementById('inner');
 const body = document.getElementsByTagName('body')[0];
-
+const paragraphs = document.querySelectorAll('.paragraphs')
 body.style.backgroundColor="azure"
 heading.style.color = "red";
 heading.style.fontWeight="500"
@@ -21,3 +21,7 @@ inner.innerHTML=
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda incidunt aspernatur quo eaque aliquid quasi reprehenderit totam quidem recusandae eveniet voluptatem eius voluptates, mollitia necessitatibus at provident dolores. Exercitationem delectus inventore quam hic numquam ex aperiam, debitis reprehenderit culpa dicta quia optio facere animi tenetur! Nihil, dolor harum maiores corporis sit laborum ex quisquam ut fugiat voluptatibus cum dignissimos totam dolorem expedita odit saepe. Perferendis sint eaque dolorum eligendi ex, corporis error voluptate inventore pariatur? Commodi iure fugiat necessitatibus dolores blanditiis voluptatibus itaque iste.</P
 <strong>end of paragraph</strong>
 `
+
+for(let para of paragraphs){
+    para.style.color="#ffb703"
+}
--- NagarroWebDEVBOOTCAMP/Lecture 12/counter/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 12/counter/app.js
@@ -3,11 +3,9 @@ let count = 0;
 function myCounter (){
 count++;
 countPrint.innerText=count
-console.log(count);
 }
 
 function myCounterdec (){
     count--;
     countPrint.innerText=count
-    console.log(count);
     }
