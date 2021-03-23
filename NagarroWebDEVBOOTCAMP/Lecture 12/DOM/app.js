2595ef72f13779f918e6d4b9d78162f97729e5a9
prateek khosla <prateekk96.com@gmail.com>
2021-03-24 05:25:28 +0530

    day 38 nagarro Lecture 12

--- NagarroWebDEVBOOTCAMP/Lecture 12/DOM/app.js
+++ NagarroWebDEVBOOTCAMP/Lecture 12/DOM/app.js
@@ -21,7 +21,25 @@ inner.innerHTML=
 Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda incidunt aspernatur quo eaque aliquid quasi reprehenderit totam quidem recusandae eveniet voluptatem eius voluptates, mollitia necessitatibus at provident dolores. Exercitationem delectus inventore quam hic numquam ex aperiam, debitis reprehenderit culpa dicta quia optio facere animi tenetur! Nihil, dolor harum maiores corporis sit laborum ex quisquam ut fugiat voluptatibus cum dignissimos totam dolorem expedita odit saepe. Perferendis sint eaque dolorum eligendi ex, corporis error voluptate inventore pariatur? Commodi iure fugiat necessitatibus dolores blanditiis voluptatibus itaque iste.</P
 <strong>end of paragraph</strong>
 `
-
+paragraphs[0].style.fontSize="22px"
 for(let para of paragraphs){
     para.style.color="#ffb703"
 }
+const imp = document.querySelector('input')
+imp.setAttribute('type','color')
+imp.getAttribute('name')
+
+
+const img = document.querySelector('img');
+
+let isTrue =false;
+setInterval(()=>{
+    if(isTrue){
+        img.setAttribute('src','https://images.unsplash.com/photo-1616277434249-1ea8218b973d?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60')
+    }
+    else{
+        img.setAttribute('src','https://images.unsplash.com/photo-1616521940317-9fb8fff3e75e?ixid=MXwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fA%3D%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=700&q=60')
+    }
+    
+    isTrue=!isTrue
+},1500)
