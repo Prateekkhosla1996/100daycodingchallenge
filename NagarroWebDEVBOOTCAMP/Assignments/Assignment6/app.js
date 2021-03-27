dfbd19dc4a161054958696eb961d6ba7f373ade1
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-03-27 08:32:13 +0530

    day 41 assignment 6

--- NagarroWebDEVBOOTCAMP/Assignments/Assignment6/app.js
+++ NagarroWebDEVBOOTCAMP/Assignments/Assignment6/app.js
@@ -8,9 +8,11 @@ addButton.addEventListener('click',(e)=>{
     // adding a todo
     let inp_text = inp_Add.value;
     console.log(inp_text)
-    var singleList = document.createElement('li');
+    var singleList = document.createElement('div');
+
     singleList.innerText = inp_text;
     singleList.classList.add('col');
+    singleList.classList.add('li');
     singleList.classList.add('col-md-8');
     // singleList.classList.add('text-center');
     list.appendChild(singleList);
@@ -22,13 +24,15 @@ addButton.addEventListener('click',(e)=>{
     deleteButton.innerHTML=`<i class="fas fa-trash-alt"></i> DELETE`;
     deleteButton.classList.add('btn');
     deleteButton.classList.add('btn-danger');
-    
+    deleteButton.classList.add('btn-md');
     // update button create
     let updateButton = document.createElement('button');    
     updateButton.innerHTML=`<i class="fas fa-edit"></i> UPDATE`;
     updateButton.classList.add('btn');
     updateButton.classList.add('btn-warning');
+    updateButton.classList.add('btn-md');
     rightDiv.classList.add('col');
+    rightDiv.classList.add('rght');
     rightDiv.classList.add('col-md-4');
     rightDiv.appendChild(deleteButton);
     rightDiv.appendChild(updateButton);
@@ -36,12 +40,17 @@ addButton.addEventListener('click',(e)=>{
     // deleting a child
     deleteButton.onclick=function(){
         list.removeChild(singleList)
+        rightDiv.classList.remove('col')
+        rightDiv.classList.remove('col-md-4')
+        rightDiv.classList.remove('rght');
         deleteButton.remove()
+        updateButton.remove()
     }
     // marking a task as complete
-    singleList.onclick=function(){
+    singleList.onclick=function(e){
         singleList.classList.toggle('underline');
     }
+
 })
 
 
