a72ba69f44e7c6972f69322241ea93e4841cbb84
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-03-29 05:25:06 +0530

    day 43 jquery

---
+++ NagarroWebDEVBOOTCAMP/Lecture 14/jquery/app.js
@@ -0,0 +1,9 @@
+$('p').css('text-align','justify');
+$('p').css('color','red');
+$('p').css('padding','5px');
+$('p').css('border','5px solid yellow');
+$('h1').css('font-weight','500')
+$('h1').css('text-align','center');
+
+$('img').css('width','250px')
+
