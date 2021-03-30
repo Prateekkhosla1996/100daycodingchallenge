9863ad457ca1566de1c39573d9bb66e3cd38b1f4
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-03-31 02:30:49 +0530

    day 45 span find element

--- DSalgo/Array/src/Array/FindElement.java
+++ DSalgo/Array/src/Array/FindElement.java
@@ -1,5 +1,27 @@
 package Array;
+import java.io.*;
+import java.util.*;
 
 public class FindElement {
 
-}
+	public static void main(String[] args) throws Exception {
+	    // write your code here
+	     Scanner sc = new Scanner(System.in);
+		    int n = sc.nextInt();
+		    int arr[] = new int[n];
+		    
+		    for(int i=0;i<arr.length;i++){
+		        arr[i]=sc.nextInt();
+		    }
+		    int x = sc.nextInt();
+		    System.out.println(find(arr,x));
+	 }
+	    public static int find(int arr[],int x){
+	        for(int i=0;i<arr.length;i++){
+	            if(arr[i]==x){
+	                return i;
+	            }
+	        }
+	        return -1;
+	    }
+	}
