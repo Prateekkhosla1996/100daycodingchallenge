d1629cf0c3cc4c12b2c34656a3fe10c6813b963b
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-04-02 20:32:11 +0530

    day 47 lecture 16

--- NagarroWebDEVBOOTCAMP/Lecture16/nodeinfo/index.js
+++ NagarroWebDEVBOOTCAMP/Lecture16/nodeinfo/index.js
@@ -1,10 +1,94 @@
 
-function fib(n){
-    if(n==0||n==1){
-        return n;
+// function fib(n){
+//     if(n==0||n==1){
+//         return n;
+//     }
+//     return fib(n-1)+fib(n-2);
+// }
+
+// console.log(fib(10));
+
+// setTimeout(()=>{
+//     console.log(process.argv)
+// },1000)
+
+// setTimeout(()=>{
+//     console.log(process.pid)
+// },2000)
+
+
+// setTimeout(()=>{
+//     console.log(process.cwd)
+// },3000)
+
+function fizzbuzz(n){
+    for(let i=1;i<=n;i++){
+        if(i%3==0&&i%5==0){
+            console.log('fizzBuzz');
+
+        }else if(i%3==0){
+            console.log('fizz')
+        }else if(i%5==0){
+            console.log('Buzz')
+        }else{
+            console.log(i)
+        }
     }
-    return fib(n-1)+fib(n-2);
 }
 
-console.log(fib(10));
+function fizzbuzz2(n){
+    for(let i=1;i<=n;i++){
+        if(i%3==0&&i%5==0){
+            console.log('fizzBuzz');
+            
+        }if(i%3==0){
+            console.log('fizz')
+           
+        }if(i%5==0){
+            console.log('Buzz')
+            return;
+        }
+            console.log(i)
+    }
+}
+// fizzbuzz2(10)
+// fizzbuzz(10)
 
+function fizzbuzzopti(n){
+    for(let i=0;i<=n;i++){
+        let str ="";
+        if(i%3==0){
+            str+='fizz';
+        }
+        if(i%5==0){
+            str+='buzz';
+        }
+        if(str==''){
+            str+=i;
+        }
+        console.log(str)
+    }
+}
+fizzbuzzopti(10)
+
+function fizzbuzzEfficient(n){
+    let c3 =0;
+    let c5=0;
+    for(let i=0;i<=n;i++){
+        let str = "";
+        if(c3===3){
+            str+='fizz';
+            c3=0;
+        }
+        if(c5===5){
+            str+='Buzz';
+            c5=0;
+        }
+        if(str==''){
+            str+=i;
+        }
+        c3++;
+        c5++;
+    }
+}
+fizzbuzzEfficient(10);
