16d58dc7c92edd4e6661c9fd189f132be059a4bb
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-04-07 21:08:03 +0530

    day 51 Lecture 17 Express Demo

--- NagarroWebDEVBOOTCAMP/Lecture17/ExpressDemo/index.js
+++ NagarroWebDEVBOOTCAMP/Lecture17/ExpressDemo/index.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const col = require('colors')
 const app = express();
-
+const path = require('path')
 
 app.get('/',(req,res)=>{
-    res.send(`<h1 style="color:lime;">Hello from Node Express</h1>`)
+    res.send(`<h1>welcome to node Express</h1>`)
+})
+app.get('/a',(req,res)=>{
+    res.sendFile(path.join(__dirname+'/index.html'));
 })
 app.listen(8000,(err)=>{
     if(err) console.log(err);
