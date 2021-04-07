8dd7e049feea755b76ee0c961d4b9cffbf4688fc
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-04-07 20:33:42 +0530

    day 51 Lecture 17 Express Demo

---
+++ NagarroWebDEVBOOTCAMP/Lecture17/ExpressDemo/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const col = require('colors')
+const app = express();
+app.get('/',(req,res)=>{
+    res.send(`<h1 style="color:lime;">Hello from Node Express</h1>`)
+})
+app.listen(8000,(err)=>{
+    if(err) console.log(err);
+    console.log(`server is listened at port : 8000`.rainbow)
+})
