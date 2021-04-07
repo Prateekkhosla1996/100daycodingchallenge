a18ebde10a8c2a795715828b8285eeb4f306432a
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-04-07 20:52:26 +0530

    day 51 Lecture 17 Express Demo

--- NagarroWebDEVBOOTCAMP/Lecture17/ExpressDemo/index.js
+++ NagarroWebDEVBOOTCAMP/Lecture17/ExpressDemo/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const col = require('colors')
 const app = express();
+
+
 app.get('/',(req,res)=>{
     res.send(`<h1 style="color:lime;">Hello from Node Express</h1>`)
 })
