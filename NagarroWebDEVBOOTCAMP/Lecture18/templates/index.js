ca375ab2d716da3d15600c248d5e5fd27fa46bbc
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-04-09 20:49:11 +0530

    day 53 Lecture 18

--- NagarroWebDEVBOOTCAMP/Lecture18/templates/index.js
+++ NagarroWebDEVBOOTCAMP/Lecture18/templates/index.js
@@ -1,10 +1,21 @@
 const express = require('express');
 const app = express();
-
+const path = require('path')
 app.set('view engine','ejs');
-
+app.set('views',path.join(__dirname,'/views'));
+//set static files
+app.use(express.static('public'))
+const todos=["go to gym", "learn web","practice ds and algo","practice for interview"]
 app.get('/',(req,res)=>{
-    res.render('./index.ejs')
+    const title='home'
+    let randomNum = Math.floor(Math.random()*10)+1;
+    res.render('./index.ejs',{title:title,random:randomNum})
+})
+
+app.get('/todo',(req,res)=>{
+    const title='todo'
+    let randomNum = Math.floor(Math.random()*10)+1;
+    res.render('./todo.ejs',{title:title,todos:todos})
 })
 
 app.listen(3000,()=>{
