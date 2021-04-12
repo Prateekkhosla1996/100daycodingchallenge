5b83bf0db9d214c959673445f9cbdcaf33875901
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-04-12 20:15:26 +0530

    day 56 Lecture19

--- NagarroWebDEVBOOTCAMP/Lecture19/restfulrouting/index.js
+++ NagarroWebDEVBOOTCAMP/Lecture19/restfulrouting/index.js
@@ -4,16 +4,6 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.set('view engine','ejs');
-app.get('/user',(req,res)=>{
-    res.send('hello from')
-})
-app.get('/',(req,res)=>{
-    res.render('index.ejs')
-})
-app.post('/user',(req,res)=>{
-    const {username}=req.body
-    res.send(`post request ${username}`)
-})
 
 app.listen(3000,()=>{
     console.log(`app is served at 3000`);
