061a542d7982c62bbc0a16d2ea5bf6417df4733b
Prateek Khosla <53938174+Prateekkhosla1996@users.noreply.github.com>
2021-04-17 04:15:40 +0530

    day 60  java question

---
+++ DSalgo/Array/src/javainerviewQuestions/GCD.java
@@ -0,0 +1,22 @@
+package javainerviewQuestions;
+
+public class GCD {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 3;
+		int b= 7;
+		System.out.println(gcd(a,b));
+	}
+	
+	public static int gcd(int a,int b) {
+		int res = Math.min(a, b);
+		while(res>0) {
+			if(a%res==0 && b%res==0) {
+				break;
+			}
+			res--;
+		}
+		return res;
+	}
+}
